ce2a03b08940cf78e66234301fb7922da4f71027
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2021-10-12 15:00:36 -0400

    Book, Genre and Author models added

--- book_manager/models.py
+++ book_manager/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse 
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, help_text='Enter a book genre')
+
+    def get_absolute_url(self):
+        return reverse('genre-info', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField('Birth', null=True, blank=True)
+    date_of_death = models.DateField('Death', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-info', args=[str(self.id)])
+    
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+    
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    synopsis = models.TextField(max_length=1000, help_text='A summary of the book content.')
+    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13-digit ISBN')
+    price = models.DecimalField('Price', max_digits=3, decimal_places=2)
+    quantity = models.IntegerField('Quantity', help_text='Quanity in stock.')
+    genre = models.ForeignKey(Genre, help_text='Select genre.' , on_delete=models.SET_NULL, null=True)
+
+    def get_absolute_url(self):
+        return reverse('book-info', args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
+    
+
+ 
+
--- inventory_manager/urls.py
+++ inventory_manager/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('book_manager/', include('book_manager.urls')),
     path('', RedirectView.as_view(url='book_manager/', permanent=True)),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
