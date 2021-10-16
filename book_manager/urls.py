696c6bf8f6a5b49e90c248e723d505b91c4a7cf3
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2021-10-16 18:53:59 -0400

    created base templates for site

--- book_manager/urls.py
+++ book_manager/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+     path('', views.index, name='index'),
+     path('book_management', views.book_management, name='book_management'),
+     path('author_management', views.author_management, name='author_management'),
+     path('contact', views.contact, name='contact'),
 ]
--- book_manager/views.py
+++ book_manager/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+ return render(request, 'book_manager/index.html')
+
+def book_management(request):
+    return render(request, 'book_manager/book_management.html')
+
+def author_management(request):
+    return render(request, 'book_manager/author_management.html')
+
+def contact(request):
+    return render(request, 'book_manager/contact.html')
