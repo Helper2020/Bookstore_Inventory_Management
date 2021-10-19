08cdad02c1ec8a9bae10277b50f08897a5ed72b0
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2021-10-18 20:34:17 -0400

    Added author list, book list to site and more templates

--- book_manager/urls.py
+++ book_manager/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
      path('', views.index, name='index'),
-     path('book_management', views.book_management, name='book_management'),
-     path('author_management', views.author_management, name='author_management'),
-     path('contact', views.contact, name='contact'),
+     path('book_management/', views.book_management, name='book-management'),
+     path('book_catalog/', views.BookCatalog.as_view(), name='book-catalog'),
+     path('book_catalog/<int:pk>', views.BookInfo.as_view(), name='book-info'),
+     path('author_management/', views.author_management, name='author-management'),
+     path('author_catalog/', views.AuthorCatalog.as_view(), name='author-catalog'),
+     path('author_catalog/<int:pk>', views.AuthorInfo.as_view(), name='author-info'),
+     path('contact/', views.contact, name='contact'),
 ]
--- book_manager/views.py
+++ book_manager/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render
+from .models import Book, Author
+from django.views import generic
 
 # Create your views here.
 
+
+class BookCatalog(generic.ListView):
+    model = Book
+    context_object_name = 'book_catalog'
+    template_name = 'book_manager/bookcatalog.html'
+
+    def get_queryset(self):
+        return Book.objects.order_by('title')
+
+class BookInfo(generic.DetailView):
+    model = Book
+    template_name = 'book_manager/bookinfo.html'
+    
+class AuthorCatalog(generic.ListView):
+    model = Author
+    context_object_name = 'author_catalog'
+    template_name = 'book_manager/authorcatalog.html'
+    
+    def get_queryset(self):
+        return Author.objects.order_by('last_name')
+
+class AuthorInfo(generic.DetailView):
+    model = Author
+    template_name = 'book_manager/authorinfo.html'
+
 def index(request):
- return render(request, 'book_manager/index.html')
+    num_books = Book.objects.all().count()
+    num_authors =  Author.objects.all().count()
+
+    info = {
+        'num_books': num_books,
+        'num_authors': num_authors
+    }
+    
+    return render(request, 'book_manager/index.html', context=info)
 
 def book_management(request):
     return render(request, 'book_manager/book_management.html')
