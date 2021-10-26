43d098f6f3edf4d1458b6c2efbaca5718e4ab996
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2021-10-25 22:21:46 -0400

    Added search box to authorinfo/search, bookinfo/search

--- inventory_manager/settings.py
+++ inventory_manager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r1^o226nc+$alk23i+!#wks-v^l#^mm$_-vae+mz^=2#=+psr)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
